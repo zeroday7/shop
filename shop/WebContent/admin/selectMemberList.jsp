65002f3bdcfdb6c875eccda567c54b1e0a076903
JavaOpenClass <jjdev@naver.com>
2021-09-15 17:18:57 +0900

    selectMemberList.jsp 검색기능추가, MemberDao selectMemberListAllBySearchMemberId메서드 추가, ISSUE해결(totalCount이슈)

--- shop/WebContent/admin/selectMemberList.jsp
+++ shop/WebContent/admin/selectMemberList.jsp
@@ -1,83 +1,146 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ page import = "vo.*" %>
-<%@ page import = "dao.*" %>
-<%@ page import = "java.util.*" %>
-<%
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="vo.*" %>
+<%@ page import="dao.*" %>
+<%@ page import="java.util.*" %>
+<%	
+	//한글인코딩
 	request.setCharacterEncoding("utf-8");
+	//사용자(일반 회원)들 리스트는 관리자만 출입
+	//방어코드
 	Member loginMember = (Member)session.getAttribute("loginMember");
-	if(loginMember == null || loginMember.getMemberLevel() < 1) {
+	//로그인 멤버값이 없거나 memberLevel이 1미만(일반 사용자)일때는 접근 불가. 순서를 바꾸면안됨(바꾸면 null포인트 인셉션이 일어남).
+	if(loginMember==null || loginMember.getMemberLevel() < 1){
 		response.sendRedirect(request.getContextPath()+"/index.jsp");
-		return;
-	} 
+		return;	//없으면 아래 ~~님 반갑습니다 쪽에 오류
+	}
 	
+	// 검색어
+	String searchMemberId = "";
+	if(request.getParameter("searchMemberId")!=null){
+		searchMemberId = request.getParameter("searchMemberId");
+	}
+	System.out.println(searchMemberId+" <--selectMemberList searchMemberId");
+	
+	//페이지
 	int currentPage = 1;
-	if(request.getParameter("currentPage") != null) {
+	//currentPage는 값이 들어오면 1대신 그 값으로 바뀜
+	if(request.getParameter("currentPage")!=null){
 		currentPage = Integer.parseInt(request.getParameter("currentPage"));
 	}
-	final int ROW_PER_PAGE = 10; // rowPerPage변수 10으로 초기화되면 끝까지 10이다. --> 상수
+	System.out.println(currentPage+" <--selectMemberList currentPage");
+	//상수(fianl) <== rowPerPage변수 10으로 초기화되면 끝까지 10이다(변하지 않음),대문자와 스네이크 표현식으로 표시한 이유는 다른사람에게 식별하기 쉽게 하기 위해서 사용
+	final int ROW_PER_PAGE = 10;
 	
-	int beginRow = (1-currentPage)*ROW_PER_PAGE;
-	
+	int beginRow = (currentPage-1)*ROW_PER_PAGE;
+	 
 	MemberDao memberDao = new MemberDao();
-	ArrayList<Member> memberList = memberDao.selectMemberListAllByPage(beginRow, ROW_PER_PAGE);
+	ArrayList<Member> memberList = null;
+	if(searchMemberId.equals("") == true) { // 검색어가 없을때
+		memberList = memberDao.selectMemberListAllByPage(beginRow, ROW_PER_PAGE);
+	} else { // 검색어가 있을때
+		memberList = memberDao.selectMemberListAllBySearchMemberId(beginRow, ROW_PER_PAGE, searchMemberId);
+	}
 
+	int totalCount = memberDao.totalMemberCount();
 %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>회원 목록</title>
+<title>사용자(회원) 목록</title>
 </head>
 <body>
+	<!-- 관리자 메뉴 인클루드(include)시작 ,페이지 형태만 인클루드 하는 것이 좋음(코드 형태는 비추천).-->
 	<div>
-		<!-- ./같은위치/partial폴더/submenu.jsp(webContent,상대주소) , /shop/partial/submenu.jsp(프로젝트기준,절대주소),/partial/submenu.jsp(절대주소)-->
 		<jsp:include page="/partial/adminMenu.jsp"></jsp:include><!-- jsp액션태그 -->
 	</div>
+	<!-- 관리자 메뉴 인클루드 끝 -->
 	<h1>회원 목록</h1>
 	<table border="1">
 		<thead>
 			<tr>
-				<th>memberNo</th>
-				<th>memberLevel</th>
-				<th>memberName</th>
-				<th>memberAge</th>
-				<th>memberGender</th>
-				<th>updateDate</th>
-				<th>createDate</th>
-				<th></th>
-			</tr>	
+			 	<th>memberNo</th>
+			 	<th>memberId</th>
+			 	<th>memberLevel</th>
+			 	<th>memberName</th>
+			 	<th>memberAge</th>
+			 	<th>memberGender</th>
+			 	<th>updateDate</th>
+			 	<th>createDate</th>
+			</tr>
 		</thead>
 		<tbody>
 			<%
-				for(Member m : memberList) {
+				for(Member m : memberList){
 			%>
 					<tr>
-						<td></td>
+						<td><%=m.getMemberNo()%></td>
+						<td><%=m.getMemberId()%></td>
 						<td>
-							<%=m.getMemberLevel()%>
+							<%=m.getMemberLevel() %>
+							<%	//level이 0,1로 출력되니 일반 회원,관리자 계정으로 출력되도록 함
+								if(m.getMemberLevel()==0){
+							%>
+									<span>일반 회원</span>
 							<%
-								if(m.getMemberLevel() == 0) {
+								}else if(m.getMemberLevel()==1){
 							%>
-									<span>일반회원</span>
-							<%		
-								} else if(m.getMemberLevel() == 1) {
-							%>
-									<span>관리자</span>
-							<%		
+									<span>관리자 계정</span>
+							<%
 								}
 							%>
 						</td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
+						<td><%=m.getMemberName()%></td>
+						<td><%=m.getMemberAge()%></td>
+						<td><%=m.getMemberGender()%></td>
+						<td><%=m.getUpdateDate()%></td>
+						<td><%=m.getCreateDate()%></td>
 					</tr>
-			<%		
+			<%	
 				}
 			%>
 		</tbody>
 	</table>
+	<div>
+	<%
+		// ISSUE : 페이지 잘되었는데... 검색한후 페이징하면 안된다 -> ISSUE 해결
+		if (currentPage > 1) {
+	%>
+		<a href="<%=request.getContextPath()%>/admin/selectMemberList.jsp?currentPage=<%=currentPage-1%>&searchMemberId=<%=searchMemberId%>">이전</a>
+	<%
+		}
+	%>
+	<%		
+		int lastPage = totalCount / ROW_PER_PAGE;
+		
+		if (totalCount % ROW_PER_PAGE != 0) {
+			lastPage += 1;
+		}
+	
+		if (currentPage < lastPage) {
+	%>
+		<a href="<%=request.getContextPath()%>/admin/selectMemberList.jsp?currentPage=<%=currentPage+1%>&searchMemberId=<%=searchMemberId%>">다음</a>
+	<%
+	}
+	%>
+	</div>
+	<!-- memberId로 검색 -->
+	<div>
+		<form action="<%=request.getContextPath()%>/admin/selectMemberList.jsp" method="get">
+			memberId :
+			<input type="text" name="searchMemberId">
+			<button type="submit">검색</button>
+		</form>
+	</div>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
--- shop/src/dao/MemberDao.java
+++ shop/src/dao/MemberDao.java
@@ -9,67 +9,157 @@ import java.util.ArrayList;
 import commons.DBUtil;
 import vo.Member;
 
-
 public class MemberDao {
 	
-	// [관리자] 회원목록출력 <-- 검색기능 추가
-	public ArrayList<Member> selectMemberListAllByPage(int beginRow, int rowPerPage) {
+	// [관리자] 회원 리스트 관리(회원목록출력)
+		public ArrayList<Member> selectMemberListAllBySearchMemberId(int beginRow, int rowPerPage, String searchMemberId) throws ClassNotFoundException, SQLException {
+			ArrayList<Member> list = new ArrayList<Member>();
+			System.out.println(beginRow + "<<beinRow");
+			System.out.println(rowPerPage + "<<rowPerPage");
+			/*
+			SELECT 
+			 	member_no memberNo, 
+			 	member_id memberId, 
+			 	member_level memberLevel,
+			 	member_name memberName, 
+			 	member_age memberAge, 
+			 	member_gender memberGender,
+			 	update_date updateDate, 
+			 	create_date createDate 
+			 FROM member 
+			 WHERE member_id LIKE ?
+			 ORDER BY create_date DESC 
+			 LIMIT ?,?
+			 */
+			
+			// db접속 메소드 호출
+			DBUtil dbUtil = new DBUtil();
+			Connection conn = dbUtil.getConnection();
+			// 쿼리 생성 및 실행
+			String sql = "SELECT member_no memberNo, member_id memberId, member_level memberLevel, member_name memberName, member_age memberAge, member_gender memberGender, update_date updateDate, create_date createDate FROM member WHERE member_id LIKE ? ORDER BY create_date DESC LIMIT ?,?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, "%"+searchMemberId+"%");
+			stmt.setInt(2, beginRow);
+			stmt.setInt(3, rowPerPage);
+			ResultSet rs = stmt.executeQuery();
+			// 리스트에 값 넣기
+			while (rs.next()) {
+				Member member = new Member();
+				member.setMemberNo(rs.getInt("memberNo"));
+				member.setMemberId(rs.getString("memberId"));
+				member.setMemberLevel(rs.getInt("memberLevel"));
+				member.setMemberName(rs.getString("memberName"));
+				member.setMemberAge(rs.getInt("memberAge"));
+				member.setMemberGender(rs.getString("memberGender"));
+				member.setUpdateDate(rs.getString("updateDate"));
+				member.setCreateDate(rs.getString("createDate"));
+				list.add(member);
+			}
+			// 접속 종료
+			rs.close();
+			stmt.close();
+			conn.close();
+			// 값 리턴
+			return list;
+		}
+	
+	// [관리자] 회원 리스트 관리(회원목록출력)
+	public ArrayList<Member> selectMemberListAllByPage(int beginRow, int rowPerPage)
+			throws ClassNotFoundException, SQLException {
 		ArrayList<Member> list = new ArrayList<Member>();
+		System.out.println(beginRow + "<<beinRow");
+		System.out.println(rowPerPage + "<<rowPerPage");
 		/*
-		 SELECT 
-		 	member_no memberNo,
-		 	member_id memberId,
-		 	member_level memberLevel,
-		 	member_name memberName,
-		 	member_age memberAge,
-		 	member_gender memberGender,
-		 	update_date updateDate,
-		 	create_date createDate
-		 FROM member
-		 ORDER BY create_date DESC
-		 LIMIT ?, ?
+		 * SELECT member_no memberNo, member_id memberId, member_level memberLevel,
+		 * member_name memberName, member_age memberAge, member_gender memberGender,
+		 * update_date updateDate, create_date createDate FROM member ORDER BY
+		 * create_date DESC LIMIT ?,?
 		 */
-		
+		// db접속 메소드 호출
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		// 쿼리 생성 및 실행
+		String sql = "SELECT member_no memberNo, member_id memberId, member_level memberLevel, member_name memberName, member_age memberAge, member_gender memberGender, update_date updateDate, create_date createDate FROM member ORDER BY create_date DESC LIMIT ?,?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, beginRow);
+		stmt.setInt(2, rowPerPage);
+		ResultSet rs = stmt.executeQuery();
+		// 리스트에 값 넣기
+		while (rs.next()) {
+			Member member = new Member();
+			member.setMemberNo(rs.getInt("memberNo"));
+			member.setMemberId(rs.getString("memberId"));
+			member.setMemberLevel(rs.getInt("memberLevel"));
+			member.setMemberName(rs.getString("memberName"));
+			member.setMemberAge(rs.getInt("memberAge"));
+			member.setMemberGender(rs.getString("memberGender"));
+			member.setUpdateDate(rs.getString("updateDate"));
+			member.setCreateDate(rs.getString("createDate"));
+			list.add(member);
+		}
+		// 접속 종료
+		rs.close();
+		stmt.close();
+		conn.close();
+		// 값 리턴
 		return list;
 	}
-	
+
+	// 총 멤버 수
+	public int totalMemberCount() throws ClassNotFoundException, SQLException {
+		int totalCount = 0;
+		// db접속 메소드 호출
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		// 쿼리생성, 실행
+		String sql = "SELECT count(*) FROM member";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()) {
+			totalCount = rs.getInt("count(*)");
+		}
+
+		return totalCount;
+	}
+
 	// [비회원]
 	public void insertMember(Member member) throws ClassNotFoundException, SQLException {
-		
-		System.out.println(member.getMemberId()+"<<memberId");
-		System.out.println(member.getMemberPw()+"<<memberPw");
-		System.out.println(member.getMemberName()+"<<memberName");
-		System.out.println(member.getMemberAge()+"<<memberAge");
-		System.out.println(member.getMemberGender()+"<<memberGender");		
+
+		System.out.println(member.getMemberId() + "<<memberId");
+		System.out.println(member.getMemberPw() + "<<memberPw");
+		System.out.println(member.getMemberName() + "<<memberName");
+		System.out.println(member.getMemberAge() + "<<memberAge");
+		System.out.println(member.getMemberGender() + "<<memberGender");
 		/*
-		 * INSERT INTO member(member_id, member_pw,member_level,member_name,member_age,member_gender,update_date,create_date)
-		 * VALUES(?,PASSWORD(?),0,?,?,?,NOW(),NOW())
+		 * INSERT INTO member(member_id,
+		 * member_pw,member_level,member_name,member_age,member_gender,update_date,
+		 * create_date) VALUES(?,PASSWORD(?),0,?,?,?,NOW(),NOW())
 		 */
 		DBUtil dbUtil = new DBUtil();
 		Connection conn = dbUtil.getConnection();
-		String sql="INSERT INTO member(member_id, member_pw,member_level, member_name, member_age, member_gender, update_date, create_date) VALUES (?, PASSWORD(?), 0, ?, ?, ?, NOW(), NOW())";
+		String sql = "INSERT INTO member(member_id, member_pw,member_level, member_name, member_age, member_gender, update_date, create_date) VALUES (?, PASSWORD(?), 0, ?, ?, ?, NOW(), NOW())";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setString(1, member.getMemberId());
 		stmt.setString(2, member.getMemberPw());
 		stmt.setString(3, member.getMemberName());
 		stmt.setInt(4, member.getMemberAge());
 		stmt.setString(5, member.getMemberGender());
-		System.out.println(stmt+"<<stmt");
+		System.out.println(stmt + "<<stmt");
 		int row = stmt.executeUpdate();
-		if(row == 1) {
+		if (row == 1) {
 			System.out.println("입력완료");
 		}
 		stmt.close();
 		conn.close();
 	}
-	
+
 	// [회원]
 	public Member login(Member member) throws ClassNotFoundException, SQLException {
 		Member returnMember = null;
 		/*
-		 * SELECT member_no memberNo, member_id memberId, member_level memberLevel
-		 * FROM member
-		 * WHERE member_id=? AND member_pw=PASSWORD(?)
+		 * SELECT member_no memberNo, member_id memberId, member_level memberLevel FROM
+		 * member WHERE member_id=? AND member_pw=PASSWORD(?)
 		 */
 		DBUtil dbUtil = new DBUtil();
 		Connection conn = dbUtil.getConnection();
@@ -77,16 +167,16 @@ public class MemberDao {
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setString(1, member.getMemberId());
 		stmt.setString(2, member.getMemberPw());
-		System.out.println(stmt+"<<stmt");
+		System.out.println(stmt + "<<stmt");
 		ResultSet rs = stmt.executeQuery();
-		if(rs.next()) {
+		if (rs.next()) {
 			returnMember = new Member();
 			returnMember.setMemberNo(rs.getInt("memberNo"));
 			returnMember.setMemberId(rs.getString("memberId"));
 			returnMember.setMemberLevel(rs.getInt("memberLevel"));
 			return returnMember;
 		}
-		System.out.println(rs+"<<rs");
+		System.out.println(rs + "<<rs");
 		rs.close();
 		stmt.close();
 		conn.close();
